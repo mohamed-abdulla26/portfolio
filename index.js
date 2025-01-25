55634b633bb47262ed547ae28ee29a1ec88f6333
mohamed abdulla <mohamedabdullasmy@gmail.com>
2025-01-25 12:19:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function showMenu(){
+    const menuBar=document.querySelector('.sidebar');
+    menuBar.style.transform='translatex(0%)'
+   }
+   function hideMenu(){
+       const menuBar=document.querySelector('.sidebar');
+    menuBar.style.transform='translatex(100%)'
+   }
+
+   function hidebar(){
+    hideMenu();
+   }
+   
